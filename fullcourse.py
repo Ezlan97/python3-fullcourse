521ab0f427c81676e69be63efe6351230928b6a0
Ezlan97 <eizs798@gmail.com>
2019-01-22 12:40:59 +0800

    Feat | print and variable

--- fullcourse.py
+++ fullcourse.py
@@ -1,1 +1,14 @@
+#simple print
 print("Hello World!")
+print("    /|")
+print("   / |")
+print("  /  |")
+print(" /___|")
+
+#variable
+name = "john"
+age = 25
+print("There once was a man name " + name + ", ")
+print("he was " + age + " years old.")
+print("he really liked the name " + name + ", ")
+print("but didn't like being " + age + ".")
